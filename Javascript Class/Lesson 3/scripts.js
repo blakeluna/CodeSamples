06212cb20b710544b9216ec73353ebcac3ffd203
Blake Luna <blakeluna@gmail.com>
2016-05-06 11:31:50 -0500

    updating code samples

---
+++ Javascript Class/Lesson 3/scripts.js
@@ -0,0 +1,5 @@
+//Javascript file for Lesson 3 of Web Programminig with Javascript
+
+var alertTimer =
+  setInterval("document.getElementById('imageOne').src = 'images/two.gif'; alert('The number has been changed to 2');",5000);
+      
